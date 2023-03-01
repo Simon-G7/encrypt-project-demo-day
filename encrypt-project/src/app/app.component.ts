bd81ba5e2b16dc9c435a4273e662d544d03c31d5
SimonG7 <madariagariverasimonmoises@gmail.com>
2023-02-28 19:47:56 -0600

    update fields in getOne view

--- encrypt-project/src/app/app.component.ts
+++ encrypt-project/src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'encrypt-project';
-}
+  title = 'DemoProject';
+  greeting = {'id': 'XXX', 'content': 'Hello World'};
+  constructor(private app: ApiService, private http: HttpClient, private router: Router) {
+    this.app.authenticate(undefined, undefined);
+  }
+  logout() {  
+    this.http.post('logout', {}).pipe(finalize (() => {
+        this.app.authenticated = false;
+        this.router.navigateByUrl('/login');
+    })).subscribe();
+  }}
