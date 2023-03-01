d48581f71fc30ba9a031f5e9fcd696e7cfec9de7
SimonG7 <madariagariverasimonmoises@gmail.com>
2023-02-28 20:05:19 -0600

    update delete component

--- encrypt-project/src/app/endpoints/delete/delete.component.ts
+++ encrypt-project/src/app/endpoints/delete/delete.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-delete',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete.component.css']
 })
 export class DeleteComponent {
+
+   password: any;
+  id!: number;
+  secret :any;
+
+  constructor(private app: ApiService){}
+
+  doDelete(){
+    let resp = this.app.deleteOne(this.id);
+    resp.subscribe(data=>{
+      console.log(data)
+      this.secret=data
+    })
+  }
+
+  deleted(){
+    if(this.secret){
+      return true
+    }else{
+      return false
+    }
+  }
 
 }
